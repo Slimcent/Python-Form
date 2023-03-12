bb489693368a8370d7fb26dfbfcded46f5c88a71
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2023-03-12 12:27:43 +0100

    Added form controls

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import tkinter as tk
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def set_fullname():
+    print("Button clicked")
+    f_name = firstEntry.get()
+    s_name = surnameEntry.get()
+    #  fullNameEntry.insert(0, f_name + " " + s_name)
+    fullNameEntry.insert(0, f_name + " " + s_name)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+form = tk.Tk()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+form.title("My python Form")
+
+fullStringVar = tk.StringVar()
+
+firstLabel = tk.Label(form, text="First Name:")
+firstEntry = tk.Entry(form)
+
+surnameLabel = tk.Label(form, text="Surname:")
+surnameEntry = tk.Entry(form, relief="raised")
+
+fullNameLabel = tk.Label(form, text="Full Name:")
+#  fullNameEntry = tk.Entry(form)
+fullNameEntry = tk.Entry(form, textvariable=fullStringVar)
+
+btnFullName = tk.Button(form, text="Full Name", command=set_fullname)
+
+firstLabel.grid(row=0, column=0, padx=15, pady=15)
+firstEntry.grid(row=0, column=1, padx=15, pady=15)
+
+surnameLabel.grid(row=1, column=0, padx=15, pady=15)
+surnameEntry.grid(row=1, column=1, padx=15, pady=15)
+
+fullNameLabel.grid(row=2, column=0, padx=15, pady=15)
+fullNameEntry.grid(row=2, column=1, padx=15, pady=15)
+
+btnFullName.grid(columnspan=2, padx=15, pady=15)
+
+form.mainloop()  # for the form to stay on the screen and not disappear
